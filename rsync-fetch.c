04835b2a42802e33beee65a28d6b72c2acb1d4de
Wessel Dankers <wsl@fruit.je>
2018-10-31 00:04:43 +0100

    wait_for_eof, sending integers

--- rsync-fetch.c
+++ rsync-fetch.c
@@ -28,6 +28,46 @@
 
 #define orz(x) (sizeof (x) / sizeof *(x))
 
+#ifdef WORDS_BIGENDIAN
+
+#ifdef HAVE_BUILTIN_BSWAP16
+#define le16(x) ((uint16_t)__builtin_bswap16(x))
+#else
+__attribute__((const,optimize(3)))
+static inline uint16_t le16(uint16_t x) {
+	return (x << 8) | (x >> 8);
+}
+#endif
+
+#ifdef HAVE_BUILTIN_BSWAP32
+#define le32(x) ((uint32_t)__builtin_bswap32(x))
+#else
+__attribute__((const,optimize(3)))
+static inline uint32_t le32(uint32_t x) {
+	x = ((x & UINT32_C(0x00FF00FF)) << 8) | ((x & UINT32_C(0xFF00FF00)) >> 8);
+	return (x << 16) | (x >> 16);
+}
+#endif
+
+#ifdef HAVE_BUILTIN_BSWAP64
+#define le64(x) ((uint64_t)__builtin_bswap64(x))
+#else
+__attribute__((const,optimize(3)))
+static inline uint64_t le64(uint64_t x) {
+	x = ((x & UINT64_C(0x00FF00FF00FF00FF)) << 8) | ((x & UINT64_C(0xFF00FF00FF00FF00)) >> 8);
+	x = ((x & UINT64_C(0x0000FFFF0000FFFF)) << 16) | ((x & UINT64_C(0xFFFF0000FFFF0000)) >> 16);
+	return (x << 32) | (x >> 32);
+}
+#endif
+
+#else
+
+#define le16(x) (x)
+#define le32(x) (x)
+#define le64(x) (x)
+
+#endif
+
 typedef struct bucket {
 	struct bucket *next;
 	struct bucket *prev;
@@ -76,7 +116,7 @@ typedef struct RsyncFetch {
 	PyObject_HEAD
 	uint64_t magic;
 	pipestream_t stream[3];
-	size_t multiplex_length;
+	size_t multiplex_remaining;
 	int pid;
 	bool multiplex;
 } RsyncFetch_t;
@@ -159,7 +199,7 @@ static PyObject *RsyncFetch_new(PyTypeObject *subtype, PyObject *args, PyObject 
 									rf->stream[STREAM_IN].fd = in_pipe[0];
 									rf->stream[STREAM_OUT].fd = out_pipe[1];
 									rf->stream[STREAM_ERR].fd = err_pipe[0];
-									rf->multiplex_length = 0;
+									rf->multiplex_remaining = 0;
 
 									return &rf->ob_base;
 								}
@@ -363,6 +403,51 @@ static bool do_io(RsyncFetch_t *rf) {
 	return true;
 }
 
+static bool wait_for_eof(RsyncFetch_t *rf) {
+	for(;;) {
+		if(rf->stream[STREAM_IN].length) {
+			errno = EBUSY;
+			return false;
+		}
+
+		struct pollfd pfds[3];
+		for(int i = 0; i < STREAM_NUM; i++) {
+			pipestream_t *stream = &rf->stream[i];
+			if(i == STREAM_OUT) {
+				pfds[i].fd = stream->length ? stream->fd : -1;
+				pfds[i].events = POLLOUT;
+			} else {
+				pfds[i].fd = stream->fd;
+				pfds[i].events = POLLIN;
+			}
+		}
+
+		int r = poll(pfds, orz(pfds), 500);
+		if(r == -1)
+			return false;
+		if(r == 0) {
+			errno = ETIME;
+			return false;
+		}
+
+		for(int i = 0; i < orz(pfds); i++) {
+			pipestream_t *stream = &rf->stream[i];
+			if(pfds[i].revents & POLLIN)
+				if(!stream_read(rf, stream))
+					return false;
+			if(pfds[i].revents & POLLOUT)
+				if(!stream_write(rf, stream))
+					return false;
+			if(pfds[i].revents & (POLLERR|POLLHUP)) {
+				close(stream->fd);
+				stream->fd = -1;
+				if(i == STREAM_IN)
+					return true;
+			}
+		}
+	}
+}
+
 static bool stream_enqueue(RsyncFetch_t *rf, pipestream_t *stream, const char *buf, size_t len) {
 	bucket_t *tail = stream->buf_tail;
 	if(tail) {
@@ -460,6 +545,8 @@ static bool stream_dequeue(RsyncFetch_t *rf, pipestream_t *stream, char *buf, si
 }
 
 static bool stream_multiplex(RsyncFetch_t *rf, pipestream_t *stream, char *buf, size_t len) {
+	if(!rf->multiplex)
+		return stream_enqueue(rf, stream, buf, len);
 	while(len) {
 		size_t chunk = len < 0xFFFFFF ? len : 0xFFFFFF;
 		uint8_t mplex[4] = { chunk, chunk >> 8, chunk >> 16, MSG_DATA };
@@ -470,20 +557,23 @@ static bool stream_multiplex(RsyncFetch_t *rf, pipestream_t *stream, char *buf, 
 		len -= chunk;
 		buf += chunk;
 	}
+	return true;
 }
 
 static bool stream_demultiplex(RsyncFetch_t *rf, pipestream_t *stream, char *buf, size_t len) {
-	size_t multiplex_length = rf->multiplex_length;
+	if(!rf->multiplex)
+		return stream_dequeue(rf, stream, buf, len);
+	size_t multiplex_remaining = rf->multiplex_remaining;
 	for(;;) {
-		if(multiplex_length < len) {
-			if(multiplex_length) {
-				if(!stream_dequeue(rf, stream, buf, multiplex_length))
+		if(multiplex_remaining < len) {
+			if(multiplex_remaining) {
+				if(!stream_dequeue(rf, stream, buf, multiplex_remaining))
 					return false;
-				buf += multiplex_length;
-				len -= multiplex_length;
+				buf += multiplex_remaining;
+				len -= multiplex_remaining;
 			}
 		} else {
-			rf->multiplex_length = multiplex_length - len;
+			rf->multiplex_remaining = multiplex_remaining - len;
 			return stream_dequeue(rf, stream, buf, len);
 		}
 
@@ -492,16 +582,16 @@ static bool stream_demultiplex(RsyncFetch_t *rf, pipestream_t *stream, char *buf
 			if(!stream_dequeue(rf, stream, (char *)mplex, sizeof mplex))
 				return false;
 			uint8_t channel = mplex[3];
-			size_t multiplex_length = mplex[0] | mplex[1] << 8 | mplex[2] << 16;
+			size_t multiplex_remaining = mplex[0] | mplex[1] << 8 | mplex[2] << 16;
 
 			if(channel == MSG_DATA)
 				break;
 
-			if(multiplex_length) {
-				char *message = malloc(multiplex_length);
+			if(multiplex_remaining) {
+				char *message = malloc(multiplex_remaining);
 				if(!message)
 					return false;
-				if(!stream_dequeue(rf, stream, message, multiplex_length)) {
+				if(!stream_dequeue(rf, stream, message, multiplex_remaining)) {
 					free(message);
 					return false;
 				}
@@ -512,6 +602,51 @@ static bool stream_demultiplex(RsyncFetch_t *rf, pipestream_t *stream, char *buf
 			}
 		}
 	}
+}
+
+static bool stream_flush(RsyncFetch_t *rf, pipestream_t *stream) {
+	while(stream->length)
+		if(!do_io(rf))
+			return false;
+	return true;
+}
+
+static bool send_int8(RsyncFetch_t *rf, int8_t d) {
+	return stream_multiplex(rf, &rf->stream[STREAM_OUT], (char *)&d, sizeof d);
+}
+
+static bool send_uint8(RsyncFetch_t *rf, uint8_t d) {
+	return stream_multiplex(rf, &rf->stream[STREAM_OUT], (char *)&d, sizeof d);
+}
+
+static bool send_int16(RsyncFetch_t *rf, int16_t d) {
+	int16_t le = le16(d);
+	return stream_multiplex(rf, &rf->stream[STREAM_OUT], (char *)&le, sizeof le);
+}
+
+static bool send_uint16(RsyncFetch_t *rf, uint16_t d) {
+	uint16_t le = le16(d);
+	return stream_multiplex(rf, &rf->stream[STREAM_OUT], (char *)&le, sizeof le);
+}
+
+static bool send_int32(RsyncFetch_t *rf, int32_t d) {
+	int32_t le = le32(d);
+	return stream_multiplex(rf, &rf->stream[STREAM_OUT], (char *)&le, sizeof le);
+}
+
+static bool send_uint32(RsyncFetch_t *rf, uint32_t d) {
+	uint32_t le = le32(d);
+	return stream_multiplex(rf, &rf->stream[STREAM_OUT], (char *)&le, sizeof le);
+}
+
+static bool send_int64(RsyncFetch_t *rf, int64_t d) {
+	int64_t le = le64(d);
+	return stream_multiplex(rf, &rf->stream[STREAM_OUT], (char *)&le, sizeof le);
+}
+
+static bool send_uint64(RsyncFetch_t *rf, uint64_t d) {
+	uint64_t le = le64(d);
+	return stream_multiplex(rf, &rf->stream[STREAM_OUT], (char *)&le, sizeof le);
 }
 
 static PyObject *RsyncFetch_readbytes(PyObject *self, PyObject *howmany_obj) {
@@ -530,7 +665,7 @@ static PyObject *RsyncFetch_readbytes(PyObject *self, PyObject *howmany_obj) {
 	if(!ret)
 		return NULL;
 
-	if(stream_dequeue(rf, &rf->stream[STREAM_IN], PyBytes_AsString(ret), howmany))
+	if(stream_demultiplex(rf, &rf->stream[STREAM_IN], PyBytes_AsString(ret), howmany))
 		return ret;
 
 	Py_DecRef(ret);
@@ -549,7 +684,7 @@ static PyObject *RsyncFetch_writebytes(PyObject *self, PyObject *bytes) {
 
 //fprintf(stderr, "%s:%d: len=%zd\n", __FILE__, __LINE__, len);
 
-	if(stream_enqueue(rf, &rf->stream[STREAM_OUT], buf, len))
+	if(stream_multiplex(rf, &rf->stream[STREAM_OUT], buf, len))
 		Py_RETURN_NONE;
 
 	return PyErr_SetFromErrno(PyExc_OSError);
